79fcad89c6c8614ce41ed6c4a22241ecacd839b0
Raihan <raihancse101@gmail.com>
2024-10-04 12:12:58 +0600

    Switch added in sevensegment for increment, decriment

--- SevenSegmentTwoDigit/sevensegmentOneDigit.asm
+++ SevenSegmentTwoDigit/sevensegmentOneDigit.asm
@@ -7,59 +7,116 @@ _main:
 	CLRF       main_i_L0+1
 ;sevensegmentOneDigit.c,4 :: 		TRISB=0x00;
 	CLRF       TRISB+0
-;sevensegmentOneDigit.c,5 :: 		portb=0xff;
+;sevensegmentOneDigit.c,5 :: 		TRISD=0xff;
+	MOVLW      255
+	MOVWF      TRISD+0
+;sevensegmentOneDigit.c,7 :: 		portb=0xff;
 	MOVLW      255
 	MOVWF      PORTB+0
-;sevensegmentOneDigit.c,6 :: 		while(1){
+;sevensegmentOneDigit.c,8 :: 		while(1){
 L_main0:
-;sevensegmentOneDigit.c,7 :: 		for(i=0;i<=9;i++){
-	CLRF       main_i_L0+0
-	CLRF       main_i_L0+1
+;sevensegmentOneDigit.c,9 :: 		if(portd.f0==0xff){
+	BTFSS      PORTD+0, 0
+	GOTO       L_main2
+;sevensegmentOneDigit.c,10 :: 		delay_ms(200);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      8
+	MOVWF      R12+0
+	MOVLW      119
+	MOVWF      R13+0
+L_main3:
+	DECFSZ     R13+0, 1
+	GOTO       L_main3
+	DECFSZ     R12+0, 1
+	GOTO       L_main3
+	DECFSZ     R11+0, 1
+	GOTO       L_main3
+;sevensegmentOneDigit.c,11 :: 		if(portd.f0==0xff){
+	BTFSS      PORTD+0, 0
+	GOTO       L_main4
+;sevensegmentOneDigit.c,12 :: 		if(i<9){
+	MOVLW      128
+	XORWF      main_i_L0+1, 0
+	MOVWF      R0+0
+	MOVLW      128
+	SUBWF      R0+0, 0
+	BTFSS      STATUS+0, 2
+	GOTO       L__main11
+	MOVLW      9
+	SUBWF      main_i_L0+0, 0
+L__main11:
+	BTFSC      STATUS+0, 0
+	GOTO       L_main5
+;sevensegmentOneDigit.c,13 :: 		i++;
+	INCF       main_i_L0+0, 1
+	BTFSC      STATUS+0, 2
+	INCF       main_i_L0+1, 1
+;sevensegmentOneDigit.c,14 :: 		}
+L_main5:
+;sevensegmentOneDigit.c,15 :: 		portb=array[i];
+	MOVF       main_i_L0+0, 0
+	ADDLW      _array+0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      PORTB+0
+;sevensegmentOneDigit.c,16 :: 		}
+L_main4:
+;sevensegmentOneDigit.c,17 :: 		}
 L_main2:
+;sevensegmentOneDigit.c,18 :: 		if(portd.f1==0xff){
+	BTFSS      PORTD+0, 1
+	GOTO       L_main6
+;sevensegmentOneDigit.c,19 :: 		delay_ms(200);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      8
+	MOVWF      R12+0
+	MOVLW      119
+	MOVWF      R13+0
+L_main7:
+	DECFSZ     R13+0, 1
+	GOTO       L_main7
+	DECFSZ     R12+0, 1
+	GOTO       L_main7
+	DECFSZ     R11+0, 1
+	GOTO       L_main7
+;sevensegmentOneDigit.c,20 :: 		if(portd.f1==0xff){
+	BTFSS      PORTD+0, 1
+	GOTO       L_main8
+;sevensegmentOneDigit.c,21 :: 		if(i>0){
 	MOVLW      128
 	MOVWF      R0+0
 	MOVLW      128
 	XORWF      main_i_L0+1, 0
 	SUBWF      R0+0, 0
 	BTFSS      STATUS+0, 2
-	GOTO       L__main7
+	GOTO       L__main12
 	MOVF       main_i_L0+0, 0
-	SUBLW      9
-L__main7:
+	SUBLW      0
+L__main12:
+	BTFSC      STATUS+0, 0
+	GOTO       L_main9
+;sevensegmentOneDigit.c,22 :: 		i--;
+	MOVLW      1
+	SUBWF      main_i_L0+0, 1
 	BTFSS      STATUS+0, 0
-	GOTO       L_main3
-;sevensegmentOneDigit.c,9 :: 		portb=array[i];
+	DECF       main_i_L0+1, 1
+;sevensegmentOneDigit.c,23 :: 		}
+L_main9:
+;sevensegmentOneDigit.c,24 :: 		portb=array[i];
 	MOVF       main_i_L0+0, 0
 	ADDLW      _array+0
 	MOVWF      FSR
 	MOVF       INDF+0, 0
 	MOVWF      PORTB+0
-;sevensegmentOneDigit.c,10 :: 		delay_ms(1000);
-	MOVLW      11
-	MOVWF      R11+0
-	MOVLW      38
-	MOVWF      R12+0
-	MOVLW      93
-	MOVWF      R13+0
-L_main5:
-	DECFSZ     R13+0, 1
-	GOTO       L_main5
-	DECFSZ     R12+0, 1
-	GOTO       L_main5
-	DECFSZ     R11+0, 1
-	GOTO       L_main5
-	NOP
-	NOP
-;sevensegmentOneDigit.c,7 :: 		for(i=0;i<=9;i++){
-	INCF       main_i_L0+0, 1
-	BTFSC      STATUS+0, 2
-	INCF       main_i_L0+1, 1
-;sevensegmentOneDigit.c,12 :: 		}
-	GOTO       L_main2
-L_main3:
-;sevensegmentOneDigit.c,14 :: 		}
+;sevensegmentOneDigit.c,25 :: 		}
+L_main8:
+;sevensegmentOneDigit.c,26 :: 		}
+L_main6:
+;sevensegmentOneDigit.c,34 :: 		}
 	GOTO       L_main0
-;sevensegmentOneDigit.c,17 :: 		}
+;sevensegmentOneDigit.c,37 :: 		}
 L_end_main:
 	GOTO       $+0
 ; end of _main
--- SevenSegmentTwoDigit/sevensegmentOneDigit.cp
+++ SevenSegmentTwoDigit/sevensegmentOneDigit.cp
@@ -3,14 +3,34 @@ char array[]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90};
 void main() {
  int i=0;
  TRISB=0x00;
+ TRISD=0xff;
+
  portb=0xff;
  while(1){
- for(i=0;i<=9;i++){
-
- portb=array[i];
- delay_ms(1000);
-
+ if(portd.f0==0xff){
+ delay_ms(200);
+ if(portd.f0==0xff){
+ if(i<9){
+ i++;
  }
+ portb=array[i];
+ }
+ }
+ if(portd.f1==0xff){
+ delay_ms(200);
+ if(portd.f1==0xff){
+ if(i>0){
+ i--;
+ }
+ portb=array[i];
+ }
+ }
+
+
+
+
+
+
 
  }
 
